5571a5024ef6ad94126af8ddd7606389ca41531e
paolatocchiotec <paolatocchiotec@gmail.com>
2026-09-13 12:45:36 -0300

    Update consultas.sql

--- banco-de-dados/consultas.sql
+++ banco-de-dados/consultas.sql
@@ -19,3 +19,16 @@ SELECT ID, post_title
 FROM wp_posts
 WHERE post_type = 'product'
 AND post_status = 'publish';
+
+
+
+-- Consulta dos produtos com seus preços
+SELECT
+    p.ID,
+    p.post_title,
+    pm.meta_value AS preco
+FROM wp_posts p
+INNER JOIN wp_postmeta pm
+    ON p.ID = pm.post_id
+WHERE p.post_type = 'product'
+AND pm.meta_key = '_price';
